608cbddedc317c690f3821d61ecbeac182d48e6d
Mikhail <me@reze.moe>
2024-04-03 17:09:32 +0900

    Implement databse interface

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 import { Server } from "@web/server";
+import DatabaseInterface from "@db/database";
 
-const server: Server = new Server();
+(async () => {
+    
+    const server: Server = new Server();
+    const database: DatabaseInterface = new DatabaseInterface(process.env.MONGO_HOST as string,
+        parseInt(process.env.MONGO_PORT as string),
+        process.env.MONGO_DB as string,
+        process.env.MONGO_USER as string,
+        process.env.MONGO_PASSWORD as string);
+    await database.connect();
+    
+    server.start();
 
-server.start();
+})().catch(console.error);
