0c100d75f035302f00455d125022012703455557
Mikhail <me@reze.moe>
2024-04-18 02:30:57 +0900

    Make it so you cant click through the loader, cause it's annoying

--- public/js/all.js
+++ public/js/all.js
@@ -3,9 +3,16 @@ var FUTABA_PARAMS = {};
 
 const FutabaParam = (param) => { return FUTABA_PARAMS[param] ?? 'Unspecified' };
 window.lazyFunctions = {
-    hideLoader: function (element)
+    hideLoader: async function (element)
     {
-        anime({targets: '.loading-video', opacity: 0, duration: 600});
+        await anime({
+            targets: '.loading-video',
+            opacity: 0,
+            duration: 600,
+            complete: () => {
+                document.querySelector('.loading-video').style.display = 'none';
+            }
+        });
     }
 };
 
@@ -109,6 +116,7 @@ const FUTABA_SCENARIOS = {
                     }
 
                     FUTABA_PARAMS.username = userInfoJson.data.user.displayName;
+                    setCookie('auth.user.key', accessKey, 365);
                     SetScenario("WELCOME_USER");
                 };
             }
@@ -276,4 +284,27 @@ function executeLazyFunction(element) {
   var lazyFunction = window.lazyFunctions[lazyFunctionName];
   if (!lazyFunction) return;
   lazyFunction(element);
+}
+
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {   
+    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
